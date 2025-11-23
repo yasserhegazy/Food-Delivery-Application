7272f7f770ecdfb9992fdf94e855e0616d227cb5
Yasser Hegazy <yasseranahegazy@gmail.com>
2025-11-23 14:39:54 +0200

    Add Address model and migration for delivery addresses

---
+++ app/Models/Address.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'label',
+        'address_line_1',
+        'address_line_2',
+        'city',
+        'zip_code',
+        'is_default',
+    ];
+
+    protected $casts = [
+        'is_default' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
